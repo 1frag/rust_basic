aa035e5e8313e10b416ab08a33171e7acd0e334b
Aleksei Piskunov <piskunov.alesha@gmail.com>
2020-12-11 10:12:17 +0700

    Added getting sum of numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 || args[1] == "hello" {
+        println!("Hello, world!");
+    }
+
+    else if args[1] == "sum" {
+        let mut res = 0;
+        for i in 2..args.len() {
+            match args[i].parse::<i32>() {
+                Ok(n) => res += n,
+                Err(e) => {
+                    println!("{} couldn't be parsed ({})", args[i], e);
+                },
+            }
+        }
+        println!("Sum of given numbers is {}", res);
+    }
+
+    else {
+        println!("undefined action for args[0]={}", args[1]);
+    }
 }
