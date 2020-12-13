bb7331917dd4bb78a9286dca4e7c2135d9df787b
Aleksei Piskunov <piskunov.alesha@gmail.com>
2020-12-13 18:02:52 +0700

    1455A solved. cf_preparator upgraded

--- cf_preparator/src/main.rs
+++ cf_preparator/src/main.rs
@@ -4,6 +4,8 @@ use std::fs;
 use std::env;
 use std::cmp::Ordering;
 use regex::Regex;
+use std::process::Command;
+use colored::*;
 
 fn fetch_problem_url() -> Option<String> {
     let args: Vec<String> = env::args().collect();
@@ -25,7 +27,7 @@ fn fetch_libs(contents: &str) -> String {
 
 fn fetch_block(content: &str, mark: String) -> Option<String> {
     let at_eq = |ind: usize, other: &str| -> bool {
-        return content[ind..ind+1].cmp(other) == Ordering::Equal;
+        return content[ind..ind + 1].cmp(other) == Ordering::Equal;
     };
 
     let opt_ind = content.find(&mark);
@@ -38,7 +40,10 @@ fn fetch_block(content: &str, mark: String) -> Option<String> {
 
     let (mut brackets, mut was) = (0, false);
     while !was || (brackets != 0) {
-        if at_eq(ind, "{") { brackets += 1; was = true; }
+        if at_eq(ind, "{") {
+            brackets += 1;
+            was = true;
+        }
         if at_eq(ind, "}") { brackets -= 1; }
         ind += 1;
     }
@@ -48,7 +53,7 @@ fn fetch_block(content: &str, mark: String) -> Option<String> {
 fn fetch_deps(content: &str, target: String) -> String {
     let re = Regex::new(r"use crate::\{(.+)}").unwrap();
     let cap = re.captures(&target);
-    if cap.is_none() { return "".to_string() }
+    if cap.is_none() { return "".to_string(); }
 
     return cap
         .unwrap()
@@ -72,25 +77,33 @@ fn main() {}
 {}", "{", name, "}")
 }
 
-fn get_ident(url: String) -> String {
-    url
-        .split("/")
-        .enumerate()
-        .filter(|(i, _)| i > &4)
-        .fold("".to_string(), |i, j| format!("{}{}", i, j.1))
+fn compile(source_path: &str, compile_path: &str) {
+    let cmd = format!("rustc {} -o {}", source_path, compile_path);
+    println!("{:?}", Command::new("sh")
+        .args(&["-c", &cmd])
+        .output()
+        .unwrap()
+        .stdout
+    );
+}
+
+fn copy_to_clipboard(t: &str) {
+    Command::new("bash")
+        .args(&["-c", &format!("echo -n '{}' | xsel -ib", t)])
+        .output()
+        .unwrap().stdout;
 }
 
 fn main() {
-    let problem_url_opt = fetch_problem_url();
-    if problem_url_opt.is_none() { return; }
-    let problem_url = problem_url_opt.unwrap();
-    let ident = get_ident(problem_url.clone());
+    let problem_ident_opt = fetch_problem_url();
+    if problem_ident_opt.is_none() { return; }
+    let problem_ident = problem_ident_opt.unwrap();
 
     let filename = "codeforces/src/main.rs";
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
-    let mod_code_opt = fetch_block(&contents, problem_url.clone());
+    let mod_code_opt = fetch_block(&contents, problem_ident.clone());
     if mod_code_opt.is_none() {
         return println!("Couldn't parse main function");
     }
@@ -104,8 +117,15 @@ fn main() {
         make_main(&mod_code)
     );
 
-    fs::write(
-        format!("cf_preparator/gen/{}.rs", ident),
-        code
-    ).expect("Something went wrong writing the file");
+    let source_path = format!("cf_preparator/gen/{}.rs", problem_ident);
+    let compile_path = format!("cf_preparator/gen/{}", problem_ident);
+    fs::write(&source_path, code)
+        .expect("Something went wrong writing the file");
+    compile(&source_path, &compile_path);
+
+    let mut invite = format!("cd cf_preparator/gen/ && ./{} > out.txt &", problem_ident);
+    invite.push_str("& echo -ne \"\\033[0;33m\" && cat out.txt && echo -ne \"\\033[0m\" && cd -");
+
+    copy_to_clipboard(&invite);
+    println!("Check:\n{}", (&invite).blue().bold());
 }
--- codeforces/src/main.rs
+++ codeforces/src/main.rs
@@ -7,13 +7,23 @@ fn read_i32() -> i32 {
     return input.trim().parse().unwrap();
 }
 
+fn read_vec_i32() -> Vec<i32> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    return input
+        .split(" ")
+        .map(|s| s.trim().parse().unwrap())
+        .collect();
+}
+
 fn read_string() -> String {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
     return input.trim().to_string();
 }
 
-pub mod t_strange_functions { // http://codeforces.com/problemset/problem/1455/A
+// 1455A
+pub mod t_strange_functions {
     use crate::{read_i32, read_string};
 
     pub fn solve() {
@@ -24,7 +34,8 @@ pub mod t_strange_functions { // http://codeforces.com/problemset/problem/1455/A
     }
 }
 
-pub mod t_jumps { // http://codeforces.com/contest/1455/problem/B
+// 1455B
+pub mod t_jumps {
     use crate::{read_i32};
 
     fn solve_one(x: i32) -> i32 {
@@ -35,11 +46,14 @@ pub mod t_jumps { // http://codeforces.com/contest/1455/problem/B
             let mid = (left + right) >> 1;
             let sum = (mid * mid + mid) >> 1;
             if sum < x { left = mid + 1 };
-            if sum > x { right = mid - 1; last_success = mid };
-            if sum == x { return mid };
+            if sum > x {
+                right = mid - 1;
+                last_success = mid
+            };
+            if sum == x { return mid; };
         }
         let sum = (last_success * last_success + last_success) >> 1;
-        if sum == x + 1 { return last_success + 1 }
+        if sum == x + 1 { return last_success + 1; }
         return last_success;
     }
 
@@ -52,6 +66,27 @@ pub mod t_jumps { // http://codeforces.com/contest/1455/problem/B
     }
 }
 
+// 1455C
+pub mod t_ping_pong {
+    use crate::{read_i32, read_vec_i32};
+
+    fn solve_one(a: i32, b: i32) -> (i32, i32) {
+        if a == 0 { return (0, b) }
+        if a == 1 && b != 0 { return (0, b) }
+        if b == 0 { return (a, 0) }
+        return (a - 1, b);
+    }
+
+    pub fn solve() {
+        let t = read_i32();
+        for _ in 0..t {
+            let inp = read_vec_i32();
+            let (a, b) = self::solve_one(inp[0], inp[1]);
+            println!("{} {}", a, b);
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -60,10 +95,12 @@ fn main() {
     }
     let task_name = args[1].to_string();
 
-    if task_name == "1455/A" {
+    if task_name == "1455A" {
         return t_strange_functions::solve();
-    } else if task_name == "1455/B" {
+    } else if task_name == "1455B" {
         return t_jumps::solve();
+    } else if task_name == "1455C" {
+        return t_ping_pong::solve();
     } else {
         println!("Problem not found");
     }
